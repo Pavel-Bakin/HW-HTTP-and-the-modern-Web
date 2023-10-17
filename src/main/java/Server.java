fc82ab5d044f67a5bdeb7e7c6742dfe59700f124
Pavel <pashtetkadet13@gmail.com>
2023-10-17 15:21:03 +0300

    feature/form-Rework

---
+++ src/main/java/Server.java
@@ -0,0 +1,120 @@
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Server {
+    private int threadPoolSize;
+
+    public Server(int threadPoolSize) {
+        this.threadPoolSize = threadPoolSize;
+    }
+
+    public void start(int port) {
+        final var validPaths = List.of("/index.html", "/spring.svg", "/spring.png", "/resources.html", "/styles.css", "/app.js", "/links.html", "/forms.html", "/classic.html", "/events.html", "/events.js");
+
+        try (final var serverSocket = new ServerSocket(port)) {
+            // Создаем пул потоков для обработки подключений
+            ExecutorService threadPool = Executors.newFixedThreadPool(threadPoolSize);
+            while (true) {
+                try {
+                    final var socket = serverSocket.accept();
+                    // Запускаем обработку подключения в отдельном потоке из пула
+                    threadPool.execute(() -> handleConnection(socket, validPaths));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void handleConnection(Socket socket, List<String> validPaths) {
+        try (
+                final var in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                final var out = new BufferedOutputStream(socket.getOutputStream());
+        ) {
+            // read only request line for simplicity
+            // must be in the form GET /path HTTP/1.1
+            final var requestLine = in.readLine();
+            final var parts = requestLine.split(" ");
+
+            if (parts.length != 3) {
+                // just close the socket
+                socket.close();
+                return;
+            }
+
+            final var path = parts[1];
+            String modifiedPath = path;
+            // Извлекаем Query String из пути запроса
+            String queryString = "";
+            if (path.contains("?")) {
+                int index = path.indexOf("?");
+                queryString = path.substring(index + 1);
+                modifiedPath = path.substring(0, index);
+            }
+
+            if (!validPaths.contains(modifiedPath)) {
+                out.write((
+                        "HTTP/1.1 404 Not Found\r\n" +
+                                "Content-Length: 0\r\n" +
+                                "Connection: close\r\n" +
+                                "\r\n"
+                ).getBytes());
+                out.flush();
+                socket.close();
+                return;
+            }
+
+            final var filePath = Path.of(".", "public", modifiedPath);
+            final var mimeType = Files.probeContentType(filePath);
+
+            // special case for classic
+            if (modifiedPath.equals("/classic.html")) {
+                final var template = Files.readString(filePath);
+                final var content = template.replace(
+                        "{time}",
+                        LocalDateTime.now().toString()
+                ).getBytes();
+                out.write((
+                        "HTTP/1.1 200 OK\r\n" +
+                                "Content-Type: " + mimeType + "\r\n" +
+                                "Content-Length: " + content.length + "\r\n" +
+                                "Connection: close\r\n" +
+                                "\r\n"
+                ).getBytes());
+                out.write(content);
+                out.flush();
+            } else {
+                final var length = Files.size(filePath);
+                out.write((
+                        "HTTP/1.1 200 OK\r\n" +
+                                "Content-Type: " + mimeType + "\r\n" +
+                                "Content-Length: " + length + "\r\n" +
+                                "Connection: close\r\n" +
+                                "\r\n"
+                ).getBytes());
+                Files.copy(filePath, out);
+                out.flush();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
